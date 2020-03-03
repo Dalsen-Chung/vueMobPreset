ba13465af713726a1ed6cbe3f0820e801c0bcc26
钟达 <504471282@qq.com>
2020-03-03 14:45:18 +0800

    完善第一阶段preset

--- generator/tools/utils.js
+++ generator/tools/utils.js
@@ -3,7 +3,8 @@ function renderFiles(api) {
     './src/utils/dgtBridge.js': '../template/src/utils/dgtBridge.js',
     './src/utils/paramEndorse.js': '../template/src/utils/paramEndorse.js',
     './src/utils/uuidGenerator.js': '../template/src/utils/uuidGenerator.js',
-    './src/utils/verifyPlate.js': '../template/src/utils/verifyPlate.js'
+    './src/utils/verifyPlate.js': '../template/src/utils/verifyPlate.js',
+    './src/utils/idCardValidate.js': '../template/src/utils/idCardValidate.js'
   })
 }
 
